da08ccc82cb33a75b935759a54a01304f237ba6b
ffont <frederic.font@gmail.com>
2017-03-14 17:04:49 +0100

    Add taxonomy to admin

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from datasets.models import Dataset, Sound, Annotation, Vote
+from datasets.models import Dataset, Sound, Annotation, Vote, Taxonomy
 
 admin.site.register(Dataset)
 admin.site.register(Sound)
 admin.site.register(Annotation)
 admin.site.register(Vote)
+admin.site.register(Taxonomy)
