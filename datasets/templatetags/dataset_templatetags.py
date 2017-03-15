b7442c03d1c49fc3fc1dd395f3eeb3fa37277492
ffont <frederic.font@gmail.com>
2017-03-15 10:46:16 +0100

    Highlight abstract and blacklisted nodes

--- datasets/templatetags/dataset_templatetags.py
+++ datasets/templatetags/dataset_templatetags.py
@@ -6,8 +6,12 @@ register = template.Library()
 
 @register.simple_tag(takes_context=False)
 def taxonomy_node_stats(dataset, node_id):
+    node = dataset.taxonomy.get_element_at_id(node_id)
+    print(node['restrictions'], 'blacklist' in node['restrictions'])
     return {
         'num_sounds': dataset.num_sounds_per_taxonomy_node(node_id),
-        'num_parents': len(dataset.taxonomy.get_element_at_id(node_id)['parents']),
-        'num_children': len(dataset.taxonomy.get_element_at_id(node_id)['children']),
+        'num_parents': len(node['parents']),
+        'num_children': len(node['children']),
+        'is_abstract': 'abstract' in node['restrictions'],
+        'is_blacklisted': 'blacklist' in node['restrictions'],
     }
