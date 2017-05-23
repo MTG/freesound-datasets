af02c63192f2e9319e18c8a4052c0a1c9eebdd64
xavier <xavier.favory@gmail.com>
2017-05-23 18:39:56 +0200

    Simplify test with only one vote form

--- datasets/tests/test_views.py
+++ datasets/tests/test_views.py
@@ -11,36 +11,34 @@ class ContributeTest(TestCase):
     fixtures = ['datasets/fixtures/initial.json']
     
     def setUp(self):
-        create_sounds('fsd', 12)
+        create_sounds('fsd', 1)
         create_users(1)
-        create_annotations('fsd', 24)
+        create_annotations('fsd', 1)
         
     def test_save_contribute_validate_annotations_category(self):
         dataset = Dataset.objects.get(short_name='fsd')
         
         # get a node id with at least one annotation
-        node_id = Annotation.objects.filter(sound_dataset__gt=1)[0].value
+        node_id = Annotation.objects.filter(sound_dataset__gt=0)[0].value
         annotations = dataset.non_validated_annotations_per_taxonomy_node(node_id)
-        all_annotation_object_ids = annotations.values_list('id', flat=True)
-        nb_annotations = min(len(all_annotation_object_ids), 12)
+        annotation_object_id = annotations.values_list('id', flat=True)[0]
         
-        # create form data
+        # create form data with one annotation vote form
         form_data = {'form-MAX_NUM_FORMS': '1000', 
                      'category_id': node_id, 
-                     'form-INITIAL_FORMS': nb_annotations, 
-                     'form-TOTAL_FORMS': nb_annotations + 1,
+                     'form-INITIAL_FORMS': 1, 
+                     'form-TOTAL_FORMS': 2,
                      'comment': '',
                      'dataset': '1',
-                    }
-        for i in range(nb_annotations):      
-            form_data['form-{0}-visited_sound'.format(i)] = ['True', 'False'][i%2]
-            form_data['form-{0}-annotation_id'.format(i)] = str(all_annotation_object_ids[i])
-            form_data['form-{0}-vote'.format(i)] = ['1', '-1', '1'][i%3]
+                    }     
+        form_data['form-0-visited_sound'] = 'Flase'
+        form_data['form-0-annotation_id'] = str(annotation_object_id)
+        form_data['form-0-vote'] = '1'
             
         self.client.login(username='username_0', password='123456')
         
         response = self.client.post(reverse('save-contribute-validate-annotations-per-category'), data=form_data)
-        response2 = self.client.post(reverse('save-contribute-validate-annotations-per-category'), data=form_data)
+        self.client.post(reverse('save-contribute-validate-annotations-per-category'), data=form_data)
 
         # checking duplicates
         duplicate_votes = []
