87632bd343981f7f21aee23084ab5b874cac75b5
xavierfav <xavier.favory@gmail.com>
2017-06-22 17:54:13 +0200

    Add periodic task

--- freesound_datasets/celery.py
+++ freesound_datasets/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freesound_datasets.settings')
@@ -21,3 +22,18 @@ app.autodiscover_tasks('datasets', related_name='tasks')
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@app.task(bind=True)
+def run_django_management_command(self, command, *args, **kwargs):
+    print('holy moly, it works!')
+    #management.call_command(command, *args, **kwargs)
+
+
+# Configure periodic tasks here
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour="*", minute="*/2"),
+        run_django_management_command.s('renew_access_tokens'),
+name='Renew expired tokens')
