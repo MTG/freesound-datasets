7707f7128a5500f4a43948853ec496f6cdc5b62d
xavierfav <xavier.favory@gmail.com>
2017-07-12 13:22:38 +0200

    Remove migration, Change names

--- datasets/migrations/0027_profile_countdown_trustable.py
+++ datasets/migrations/0027_profile_countdown_trustable.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datasets', '0028_vote_is_trustable'),
+        ('datasets', '0026_vote_is_trustable'),
     ]
 
     operations = [
