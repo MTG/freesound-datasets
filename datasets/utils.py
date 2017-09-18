054a46f8147e2cc6d5af268388cf494e48b29495
xavierfav <xavier.favory@gmail.com>
2017-09-18 11:29:20 +0200

    Add query_freesound_by_id chunks of 50

--- datasets/utils.py
+++ datasets/utils.py
@@ -3,6 +3,8 @@ from urllib.parse import urljoin
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
+import datasets.freesound as fs
+
 
 def generate_download_script(dataset):
     access_token_url = urljoin(settings.BASE_URL, reverse('get_access_token'))
@@ -15,3 +17,20 @@ def generate_download_script(dataset):
         'get_code_url': settings.FS_CLIENT_ID
     }
     return render_to_string('datasets/download_script.py', tvars)
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def query_freesound_by_id(list_ids):
+    client = fs.FreesoundClient()
+    client.set_token(settings.FS_CLIENT_SECRET)
+    results = []
+    for sub_list_ids in chunks(list_ids, 50):
+        filter_str = 'id:(' + ' OR '.join([str(i) for i in sub_list_ids]) + ')'
+        page_result = client.text_search(query="", page_size=50, filter=filter_str)
+        results += [s for s in page_result]
+    return results
