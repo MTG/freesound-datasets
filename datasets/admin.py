8696885e9f1535bdfb8dbc0e285c67d1e6d41a95
xavierfav <xavier.favory@gmail.com>
2017-10-13 11:23:31 +0200

    Add custom Admin model TaxonomyNode, hide freesound ex

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from datasets.models import Dataset, Sound, Annotation, Vote, Taxonomy, DatasetRelease, TaxonomyNode
 
+
+class TaxonomyNodeAdmin(admin.ModelAdmin):
+    fields = ('node_id', 'name', 'description', 'citation_uri', 'faq')
+
+
 admin.site.register(Dataset)
 admin.site.register(Sound)
 admin.site.register(Annotation)
 admin.site.register(Vote)
 admin.site.register(Taxonomy)
 admin.site.register(DatasetRelease)
-admin.site.register(TaxonomyNode)
+admin.site.register(TaxonomyNode, TaxonomyNodeAdmin)
