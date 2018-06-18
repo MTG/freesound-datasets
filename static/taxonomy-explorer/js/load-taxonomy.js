01e0a350e0936cdaa3b47c91b107271fb1758f72
Lorenzo <lorenzo.m.romanelli@gmail.com>
2018-06-18 17:26:36 +0200

    Add dropdown to taxonomy tree

--- static/taxonomy-explorer/js/load-taxonomy.js
+++ static/taxonomy-explorer/js/load-taxonomy.js
@@ -31,6 +31,7 @@
 function TaxonomyTree(Options) {
     this.url = Options.url;
     this.container = Options.container;
+    this.skipCategories = Options.skipCategories || [];
     this.categories = [];
     this.ontology_tree;
     this.data;
@@ -56,25 +57,33 @@ TaxonomyTree.prototype = {
 
     buildTree: function () {
         var tt = this;
-        var node = tt.data;
-        tt.ontology_tree = tt.addNode(node);
+        var root = tt.data;
+        $(tt.container).append(
+            $("<div>", {
+                class: "ui list"
+            })
+        );
+        tt.ontology_tree = tt.addNode(root);
     },
 
     addNode: function (node, parent_name) {
 
         var tt = this;
+        var skip_this = tt.skipCategories.indexOf(node.name) > -1;
 
         var category_info = {
             name: node.name,
             parent: parent_name || null,
             children: node.children
         };
+
         var category = new Category(category_info);
-        var DOM = category.DOM;
+        var DOM = skip_this ? $(tt.container) : category.DOM;
+        // uncomment next line to flatten the tree into a list
         //tt.categories.push(category);
 
         if (category.hasChildren()) {
-            var parent_node = node;
+            var parent_node = skip_this ? tt.data : node;
             var children_DOM_container = DOM.find(".list");
             $(parent_node.children).each(function () {
                 // recursively build tree
@@ -84,7 +93,6 @@ TaxonomyTree.prototype = {
         }
 
         return DOM;
-
     },
 
     showTree: function () {
@@ -99,16 +107,13 @@ function Category(Info) {
     this.name = Info.name;
     this.parent = Info.parent;      //TODO: more than one parent???
     this.children = Info.children;
+    this.skipped = Info.skipped || false;
+    this.expanded = Info.expanded || false;
+    this.open = false;
     this.last_child = !this.hasChildren();
-    this.DOM = this.buildCategory();
-    //this.children_DOM = this.DOM.find("list");
+    this.DOM;
 
-    /*
-    this.description
-    this.players
-    this.external_link
-    ...
-     */
+    this.buildCategory();
 }
 
 Category.prototype = {
@@ -145,10 +150,16 @@ Category.prototype = {
         var category_name = ct.name;
 
         var category_DOM = $("<div>", {
-            class: "item expanded"
-            // TODO: check if last child
-            // TODO: 'expanded' and 'open' statuses
+            class: "item"
+            // TODO: 'open' status
         });
+
+        if (ct.last_child) {
+            category_DOM.addClass("last");
+        }
+        if (ct.expanded) {
+            category_DOM.addClass("expanded");
+        }
 
         var category_icon = ct.buildCategoryIcon();
 
@@ -165,6 +176,11 @@ Category.prototype = {
             children_list = $("<div>", {
                 class: "list"
             });
+            if (!ct.expanded) {
+                children_list.css({
+                    "display": "none"
+                });
+            }
         }
 
         var show_info = $("<button>", {
@@ -180,14 +196,34 @@ Category.prototype = {
         content.append([header, children_list]);
         category_DOM.append([category_icon, content]);
 
-        return category_DOM;
+        ct.DOM = category_DOM;
+    },
 
+    toggleChildren: function () {
+        var ct = this;
+        if (!ct.last_child) {
+            ct.DOM.hasClass("expanded") ? ct.closeChildren() : ct.openChildren();
+        }
+    },
+
+    openChildren: function () {
+        var ct = this;
+        ct.DOM.toggleClass("expanded");
+        var children_list = ct.DOM.children(".content").children(".list");
+        children_list.slideDown(500, "swing");
+    },
+
+    closeChildren: function () {
+        var ct = this;
+        var children_list = ct.DOM.children(".content").children(".list");
+        children_list.slideUp(500, "swing", function() {
+            ct.DOM.toggleClass("expanded");
+        });
     },
 
     buildCategoryIcon: function () {
 
         var ct = this;
-
         var class_name;
 
         if (ct.hasChildren()) {
@@ -199,6 +235,10 @@ Category.prototype = {
 
         var icon = $("<i>", {
             class: class_name
+        });
+
+        icon.click(function () {
+            ct.toggleChildren();
         });
 
         return icon
