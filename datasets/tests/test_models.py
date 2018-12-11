01acfa54c99f947885a7ce314e1a359405ff03e4
xavierfav <xavier.favory@gmail.com>
2018-12-11 19:19:07 +0100

    Add expert votes, Enable delete gt annotations Expert votes take priority for the ground truth state generation. This means they can delete some ground truth annotations. Added some process for unpropagated ground truth annotations when they have to be deleted. Some tests added to check that annotations propagates/unpropgate/update well. This commit also adds a unique contrains for the GroundTruthAnnotation model on ('taxonomy_node', 'sound_dataset',).

--- datasets/tests/test_models.py
+++ datasets/tests/test_models.py
@@ -221,3 +221,292 @@ class DatasetTest(TestCase):
     def test_user_is_maintainer(self):
         user = models.User.objects.first()
         self.assertEqual(self.dataset.user_is_maintainer(user), True)
+
+
+class GroundTruthAnnotationTest(TestCase):
+
+    def setUp(self):
+        taxonomy = {
+            "1": {
+                "id": "1",
+                "name": "1",
+                "description": "This is a root category",
+                "child_ids": ["2"],
+                "restrictions": [],
+                "citation_uri": "",
+            },
+            "2": {
+                "id": "2",
+                "name": "2",
+                "description": "This is a simple non-root & non-leaf category",
+                "child_ids": ["3"],
+                "parent_ids": ["1"],
+                "propagate_to_parent_ids": ["1"],
+                "restrictions": [],
+                "citation_uri": "",
+            },
+            "3": {
+                "id": "3",
+                "name": "3",
+                "description": "This category has two parents",
+                "child_ids": [],
+                "parent_ids": ["2", "5"],
+                "propagate_to_parent_ids": ["5"],
+                "restrictions": [],
+                "citation_uri": "",
+            },
+            "4": {
+                "id": "4",
+                "name": "4",
+                "description": "This is a leaf category",
+                "child_ids": [],
+                "parent_ids": ["5"],
+                "propagate_to_parent_ids": ["5"],
+                "restrictions": [],
+                "citation_uri": "",
+            },
+            "5": {
+                "id": "5",
+                "name": "5",
+                "description": "This is a root category",
+                "child_ids": ["3", "4"],
+                "restrictions": [],
+                "citation_uri": "",
+            },
+        }
+
+        self.taxobj = models.Taxonomy.objects.create(data=taxonomy)
+        add_taxonomy_nodes(self.taxobj)
+
+        models.Dataset.objects.create(short_name='fsd',
+                                      taxonomy=self.taxobj)
+
+        create_sounds('fsd', 1)
+        create_users(1)
+        self.sound = models.Sound.objects.first()
+        self.user = models.User.objects.first()
+
+        self.taxonomy_node = self.taxobj.get_element_at_id("3")  # a children node
+        self.second_taxonomy_node = self.taxobj.get_element_at_id("4")  # a children node
+        # a root node which is propagated from the two nodes above
+        self.taxonomy_node_to_propagate_to = self.taxobj.get_element_at_id("5")
+
+        create_candidate_annotations('fsd', 1)
+        self.candidate_annotation = models.CandidateAnnotation.objects.create(
+            sound_dataset=self.sound.sounddataset_set.first(),
+            type='AU',
+            algorithm='Fake algorithm name',
+            taxonomy_node=self.taxonomy_node,
+            ground_truth=1.0,
+        )
+
+        self.users = models.User.objects.all()
+        self.dataset = models.Dataset.objects.get(short_name='fsd')
+        # self.candidate_annotation = models.CandidateAnnotation.objects.first()
+        # self.candidate_annotation.ground_truth = 1.0
+        self.candidate_annotation.save()
+
+        self.gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=1.0,
+            created_by=self.user,
+            sound_dataset=self.sound.sounddataset_set.first(),
+            taxonomy_node=self.taxonomy_node)
+        self.gt_annotation.from_candidate_annotations.add(self.candidate_annotation)
+
+    def test_propagate_ground_truth_annotations(self):
+        self.gt_annotation.propagate_annotation()
+
+        # check that a ground truth annotation was created for taxonomy node with id 5
+        self.assertEqual(1, self.taxonomy_node_to_propagate_to.num_ground_truth_annotations)
+
+    def test_propagate_ground_truth_annotations_does_not_create_new_one(self):
+        existing_gt_value = 0.5
+        # first create a ground truth annotation in the parent node
+        existing_gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=existing_gt_value,
+            created_by=self.user,
+            sound_dataset=self.sound.sounddataset_set.first(),
+            taxonomy_node=self.taxonomy_node_to_propagate_to,
+        )
+        existing_gt_annotation.from_candidate_annotations.add(self.candidate_annotation)
+        self.gt_annotation.propagate_annotation()
+
+        # check that it did not create another ground truth annotation, neither altered it
+        num_gt_annotations = models.GroundTruthAnnotation.objects.filter(
+            taxonomy_node=self.taxonomy_node_to_propagate_to).count()
+        self.assertEqual(1, num_gt_annotations)
+        gt_annotation = models.GroundTruthAnnotation.objects.filter(
+            taxonomy_node=self.taxonomy_node_to_propagate_to).first()
+        self.assertEqual(existing_gt_value, gt_annotation.ground_truth)
+
+    def test_propagate_ground_truth_annotations_expert(self):
+        existing_gt_value = 0.5
+        expected_value_after_propagation = 1.0
+        # first create a ground truth annotation in the parent node
+        existing_gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=existing_gt_value,
+            created_by=self.user,
+            sound_dataset=self.sound.sounddataset_set.first(),
+            taxonomy_node=self.taxonomy_node_to_propagate_to,
+        )
+        existing_gt_annotation.from_candidate_annotations.add(self.candidate_annotation)
+
+        self.gt_annotation.propagate_annotation(from_expert=True)
+
+        # check that it did not create another ground truth annotation, and did altered its ground truth value
+        num_gt_annotations = models.GroundTruthAnnotation.objects.filter(
+            taxonomy_node=self.taxonomy_node_to_propagate_to).count()
+        self.assertEqual(1, num_gt_annotations)
+        gt_annotation = models.GroundTruthAnnotation.objects.filter(
+            taxonomy_node=self.taxonomy_node_to_propagate_to).first()
+        self.assertEqual(expected_value_after_propagation, gt_annotation.ground_truth)
+
+    def test_unpropagate_ground_truth_annotation(self):
+        # first create a ground truth annotation in the parent node
+        existing_gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=1,
+            created_by=self.user,
+            sound_dataset=self.sound.sounddataset_set.first(),
+            taxonomy_node=self.taxonomy_node_to_propagate_to,
+        )
+        existing_gt_annotation.from_candidate_annotations.add(self.candidate_annotation)
+
+        # unpropagate gt annotation
+        self.gt_annotation.unpropagate_annotation(self.candidate_annotation)
+
+        # check that the parent gt annotation was deleted
+        self.assertEqual(0, models.GroundTruthAnnotation.objects.filter(
+                                taxonomy_node=self.taxonomy_node_to_propagate_to).count())
+
+    def test_unpropagate_ground_truth_annotation_does_not_remove_ground_truth_if_multiple_children_annotations(self):
+        # first create candidate
+        second_candidate_annotation = models.CandidateAnnotation.objects.create(
+            sound_dataset=self.sound.sounddataset_set.first(),
+            type='AU',
+            algorithm='Fake algorithm name',
+            taxonomy_node=self.second_taxonomy_node,
+            ground_truth=1,
+        )
+        # create ground truth annotation in one of the children node and propagate it
+        existing_gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=1,
+            created_by=self.user,
+            sound_dataset=self.sound.sounddataset_set.first(),
+            taxonomy_node=self.second_taxonomy_node,
+        )
+        existing_gt_annotation.from_candidate_annotations.add(second_candidate_annotation)
+        existing_gt_annotation.propagate_annotation()
+
+        # unpropagate the ground truth annotation in the other children node
+        self.gt_annotation.unpropagate_annotation(self.candidate_annotation)
+
+        # check that the propagated gt annotation was not removed
+        self.assertEqual(
+            1,
+            models.GroundTruthAnnotation.objects.filter(taxonomy_node=self.taxonomy_node_to_propagate_to).count()
+        )
+
+
+class VoteTest(TestCase):
+    fixtures = ['datasets/fixtures/initial.json']
+
+    def setUp(self):
+        # For these tests we create only one sound and one candidate annotation
+        add_taxonomy_nodes(models.Taxonomy.objects.get())
+        create_sounds('fsd', 1)
+        create_users(2)
+        create_candidate_annotations('fsd', 1)
+        self.users = models.User.objects.all()
+        self.dataset = models.Dataset.objects.get(short_name='fsd')
+        self.candidate_annotation = models.CandidateAnnotation.objects.first()
+
+    def test_save_votes_create_ground_truth_annotation(self):
+        for user in self.users:
+            models.Vote.objects.create(
+                created_by=user,
+                vote=1.0,
+                candidate_annotation=self.candidate_annotation,
+                )
+        # check that one ground truth annotation was created (only not propagated), and that his properties are ok
+        ground_truth_annotations = models.GroundTruthAnnotation.objects.filter(from_propagation=False)
+        self.assertEqual(1, ground_truth_annotations.count())
+        ground_truth_annotation = ground_truth_annotations.first()
+        self.assertEqual(self.candidate_annotation, ground_truth_annotation.from_candidate_annotations.first())
+        self.assertEqual(self.candidate_annotation.taxonomy_node, ground_truth_annotation.taxonomy_node)
+        self.assertEqual(1.0, ground_truth_annotation.ground_truth)
+        # check that the candidate annotation has ground truth field set
+        self.assertEqual(1.0, self.candidate_annotation.ground_truth)
+
+    def test_save_one_expert_vote_creates_ground_truth_annotation(self):
+        user = self.users.first()
+        models.Vote.objects.create(
+            created_by=user,
+            vote=1.0,
+            candidate_annotation=self.candidate_annotation,
+            from_expert=True,
+        )
+        # check that one ground truth annotation was created (only not propagated), and that his properties are ok
+        ground_truth_annotations = models.GroundTruthAnnotation.objects.filter(from_propagation=False)
+        self.assertEqual(1, ground_truth_annotations.count())
+        ground_truth_annotation = ground_truth_annotations.first()
+        self.assertEqual(self.candidate_annotation, ground_truth_annotation.from_candidate_annotations.first())
+        self.assertEqual(self.candidate_annotation.taxonomy_node, ground_truth_annotation.taxonomy_node)
+        self.assertEqual(1.0, ground_truth_annotation.ground_truth)
+        # check that the candidate annotation has ground truth field set
+        self.assertEqual(1.0, self.candidate_annotation.ground_truth)
+
+    def test_save_one_expert_PNP_vote_modifies_existing_ground_truth_annotation(self):
+        user = self.users.first()
+        gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=1.0,
+            created_by=user,
+            sound_dataset=self.candidate_annotation.sound_dataset,
+            taxonomy_node=self.candidate_annotation.taxonomy_node,
+        )
+        gt_annotation.from_candidate_annotations.add(self.candidate_annotation)
+
+        models.Vote.objects.create(
+            created_by=user,
+            vote=0.5,
+            candidate_annotation=self.candidate_annotation,
+            from_expert=True,
+        )
+        # check that one ground truth annotation was created (only not propagated), and that his properties are ok
+        ground_truth_annotations = models.GroundTruthAnnotation.objects.filter(from_propagation=False)
+        self.assertEqual(1, ground_truth_annotations.count())
+        ground_truth_annotation = ground_truth_annotations.first()
+        self.assertEqual(self.candidate_annotation, ground_truth_annotation.from_candidate_annotations.first())
+        self.assertEqual(self.candidate_annotation.taxonomy_node, ground_truth_annotation.taxonomy_node)
+        self.assertEqual(0.5, ground_truth_annotation.ground_truth)
+        # check that the candidate annotation has ground truth field set
+        self.assertEqual(0.5, self.candidate_annotation.ground_truth)
+
+    def test_save_one_expert_vote_propagates_ground_truth_annotation(self):
+        pass
+        # TODO: this test needs more controllable data such as the one in GroundTruthAnnotationTest setUp taxonomy
+
+    def test_save_one_NP_expert_vote_deletes_ground_truth_annotation(self):
+        user = self.users.first()
+        gt_annotation = models.GroundTruthAnnotation.objects.create(
+            ground_truth=1.0,
+            created_by=user,
+            sound_dataset=self.candidate_annotation.sound_dataset,
+            taxonomy_node=self.candidate_annotation.taxonomy_node,
+        )
+        gt_annotation.from_candidate_annotations.add(self.candidate_annotation)
+
+        models.Vote.objects.create(
+            created_by=user,
+            vote=-1,
+            candidate_annotation=self.candidate_annotation,
+            from_expert=True,
+        )
+        # check that one ground truth annotation was created (only not propagated), and that his properties are ok
+        ground_truth_annotations = models.GroundTruthAnnotation.objects.filter(from_propagation=False)
+        self.assertEqual(0, ground_truth_annotations.count())
+        # check that the candidate annotation has ground truth field set
+        self.assertEqual(-1, self.candidate_annotation.ground_truth)
+
+    def test_save_one_NP_expert_vote_deletes_propagated_ground_truth_annotations(self):
+        pass
+        # TODO: this test needs more controllable data such as the one in GroundTruthAnnotationTest setUp taxonomy
