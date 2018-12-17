19ee73e9531387369ee45667305b06e3ce90defb
xavierfav <xavier.favory@gmail.com>
2018-12-17 17:41:43 +0100

    Add progress print

--- datasets/migrations/0056_groundtruthannotation_from_candidate_annotations.py
+++ datasets/migrations/0056_groundtruthannotation_from_candidate_annotations.py
@@ -7,8 +7,15 @@ class Migration(migrations.Migration):
 
     def add_from_candidate_annotations(apps, schema_editor):
         GroundTruthAnnotation = apps.get_model('datasets', 'GroundTruthAnnotation')
-        for ground_truth_annotation in GroundTruthAnnotation.objects.all().select_related('from_candidate_annotation'):
+        count = 0
+        ground_truth_annotations = GroundTruthAnnotation.objects.all().select_related('from_candidate_annotation')
+        num_gt_annotations = ground_truth_annotations.count()
+        for ground_truth_annotation in ground_truth_annotations:
             ground_truth_annotation.from_candidate_annotations.add(ground_truth_annotation.from_candidate_annotation)
+            sys.stdout.write('\rUpdating field %i of %i (%.2f%%)'
+                    % (count + 1, num_gt_annotations, 100.0 * (count + 1) / num_gt_annotations))
+            sys.stdout.flush()
+            count += 1
 
     dependencies = [
         ('datasets', '0055_vote_from_expert'),
