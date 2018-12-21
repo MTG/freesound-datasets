1586dffcb9e309072e7e7037080a6e867f01338d
xavierfav <xavier.favory@gmail.com>
2018-12-21 13:45:40 +0100

    Update generated taxonomy tree

--- datasets/models.py
+++ datasets/models.py
@@ -189,7 +189,7 @@ class Taxonomy(models.Model):
             if node.omitted:
                 dict_level["mark"].append("omittedTT")
             if node.omitted_curation_task:
-                node["mark"].append("omittedCurationTask")
+                dict_level["mark"].append("omittedCurationTask")
             dict_level["children"] = get_all_children(node.node_id)
             output_dict["children"].append(dict_level)
             
