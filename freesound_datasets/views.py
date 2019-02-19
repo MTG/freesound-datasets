e95b0e3cdde8abe7df52e7a66f7a250d33f35bb1
xavierfav <xavier.favory@gmail.com>
2019-02-19 20:16:15 +0100

    Remove unused variables login

--- freesound_datasets/views.py
+++ freesound_datasets/views.py
@@ -43,15 +43,9 @@ def discussion(request, short_name=None):
 def login(request):
     next_url = request.GET.get('next', '')
     show_freesound = bool(settings.SOCIAL_AUTH_FREESOUND_KEY)
-    show_github = bool(settings.SOCIAL_AUTH_GITHUB_KEY)
-    show_facebook = bool(settings.SOCIAL_AUTH_FACEBOOK_KEY)
-    show_google = bool(settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
 
     tvars = {'next': next_url,
              'show_freesound': show_freesound,
-             'show_github': show_github,
-             'show_facebook': show_facebook,
-             'show_google': show_google
              }
     return render(request, 'login.html', tvars)
 
