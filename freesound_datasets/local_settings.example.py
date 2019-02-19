1f6ba483902c59dc70d15ea1e33957ac6a874f01
xavierfav <xavier.favory@gmail.com>
2019-02-19 15:03:29 +0100

    Remove unused social auth keys

--- freesound_datasets/local_settings.example.py
+++ freesound_datasets/local_settings.example.py
@@ -9,21 +9,3 @@ FS_CLIENT_SECRET = 'FREESOUND_SECRET'
 # Set callback url to http://localhost:8000/social/complete/freesound/
 SOCIAL_AUTH_FREESOUND_KEY = None
 SOCIAL_AUTH_FREESOUND_SECRET = 'FREESOUND_SECRET'
-
-# Google keys for "login with" functionality
-# Get credentials at https://console.developers.google.com
-# Set callback url to http://localhost:8000/social/complete/google-oauth2/
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = None  # (remove the part starting with the dot .)
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'GOOGLE_SECRET'
-
-# Facebook keys for "login with" functionality
-# See instructions in https://simpleisbetterthancomplex.com/tutorial/2016/10/24/how-to-add-social-login-to-django.html
-# NOTE: might not work in localhost
-SOCIAL_AUTH_FACEBOOK_KEY = None
-SOCIAL_AUTH_FACEBOOK_SECRET = 'FACEBOOK_SECRET'
-
-# Github keys for "login with" functionality
-# Get credentials at https://github.com/settings/applications/new
-# Set callback url to http://localhost:8000/social/complete/github/
-SOCIAL_AUTH_GITHUB_KEY = None
-SOCIAL_AUTH_GITHUB_SECRET = 'GITHUB_SECRET'
